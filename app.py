84f66b905cb35d528b40a4449aa3453e7dde5317
James Dickson <jamesstuartdickson@gmail.com>
2023-07-18 15:12:04 +0100

    created new template, updated orders controller

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from controllers.orders_controller import orders_blueprint
 
+
 app = Flask(__name__)
 app.register_blueprint(orders_blueprint)
 
--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -2,7 +2,13 @@ from flask import Blueprint, render_template
 from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
+order_blueprint = Blueprint("order", __name__)
 
 @orders_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="Orders List", order_list = orders)
+
+@order_blueprint.route('/order/<index>')
+def order(index):
+    specific_order = orders[index - 1]
+    return render_template("order.html", title=f"Order: {index}", specific_order = specific_order)
