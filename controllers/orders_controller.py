1f2039b5836f288f1eb2cfff013f038f38d25f68
James Dickson <jamesstuartdickson@gmail.com>
2023-07-18 15:31:33 +0100

    updated order controller to finally make it load the order page

--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -2,13 +2,12 @@ from flask import Blueprint, render_template
 from models.order_list import orders
 
 orders_blueprint = Blueprint("orders", __name__)
-order_blueprint = Blueprint("order", __name__)
 
 @orders_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="Orders List", order_list = orders)
 
-@order_blueprint.route('/order/<index>')
+@orders_blueprint.route('/orders/<index>')
 def order(index):
-    specific_order = orders[index - 1]
+    specific_order = orders[int(index) - 1]
     return render_template("order.html", title=f"Order: {index}", specific_order = specific_order)
