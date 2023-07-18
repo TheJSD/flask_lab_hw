ed3e7716557c42bfd802f2609c6c93bcd396a5d1
James Dickson <jamesstuartdickson@gmail.com>
2023-07-18 14:29:36 +0100

    Started hw, created models

---
+++ models/order_list.py
@@ -0,0 +1,5 @@
+from models.orders import Order
+# format of the Order class: customer_name, order_date, game_title, quantity
+order1 = Order("Jason", "16/07/2023", "Bubberducky 2", 1)
+order2 = Order("Taylor", "17/07/2023", "Moogle Quest XVII", 1)
+order3 = Order("Dave", "17/07/2023", "Project Zomboid", 4)
