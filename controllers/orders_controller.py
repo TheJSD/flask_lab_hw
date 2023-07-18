e932ceea860c0977514d40295480f41eac63f94f
James Dickson <jamesstuartdickson@gmail.com>
2023-07-18 14:42:50 +0100

    updated orders controller to fix blueprint name

--- controllers/orders_controller.py
+++ controllers/orders_controller.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 from models.order_list import orders
 
-tasks_blueprint = Blueprint("orders", __name__)
+orders_blueprint = Blueprint("orders", __name__)
 
-@tasks_blueprint.route("/orders")
+@orders_blueprint.route("/orders")
 def index():
     return render_template("index.html", title="Orders List", order_list = orders)
