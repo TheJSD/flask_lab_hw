1ab7c5c65c3a1164fd7ab341d0250aa186ecd2b8
James Dickson <jamesstuartdickson@gmail.com>
2023-07-18 14:33:13 +0100

    updated orders_controller.py

--- models/order_list.py
+++ models/order_list.py
@@ -3,3 +3,5 @@ from models.orders import Order
 order1 = Order("Jason", "16/07/2023", "Bubberducky 2", 1)
 order2 = Order("Taylor", "17/07/2023", "Moogle Quest XVII", 1)
 order3 = Order("Dave", "17/07/2023", "Project Zomboid", 4)
+
+orders = [order1, order2, order3]
